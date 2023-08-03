397f9b84ca54ad33bc4c3e00a77635e10d5b4b78
Bibin <88414576+bibin567@users.noreply.github.com>
2023-08-03 15:55:52 +0100

    new commit

--- logpro.php
+++ logpro.php
@@ -1,35 +1,41 @@
 <?php
+echo "Start of PHP code.<br>";
+
 session_start();
+echo "Session started.<br>";
+
 include('connection.php');
-$obj=new connection();
+echo "Connection.php included.<br>";
 
+$obj = new connection();
+echo "Connection object created.<br>";
 
-$un=$_POST['uname'];
-$pd=$_POST['pwd'];
+$un = $_POST['uname'];
+$pd = $_POST['pwd'];
 
-$se="select * from login where username='$un' and password='$pd'";
-$ex=$obj->execute($se);
-$fe=mysqli_fetch_array($ex);
+$se = "select * from login where username='$un' and password='$pd'";
+echo "Query: $se<br>";
 
-$_SESSION['logid']=$fe[0];
-$_SESSION['uname']=$fe[1];
-$_SESSION['email']=$fe[4];
+$ex = $obj->execute($se);
+echo "Query executed.<br>";
 
-if($fe[3]=='admin')
-{
-header("location:admin/home.php");
+$fe = mysqli_fetch_array($ex);
+
+$_SESSION['logid'] = $fe[0];
+$_SESSION['uname'] = $fe[1];
+$_SESSION['email'] = $fe[4];
+
+if ($fe[3] == 'admin') {
+    header("location:admin/home.php");
+    echo "Redirecting to admin/home.php.<br>";
+} else {
+    ?>
+    <script>
+        alert('invalid user name and password');
+        window.location='login.php';
+    </script>
+    <?php
+    echo "JavaScript alert shown and redirected to login.php.<br>";
 }
-
-
-
-else
-{
-?>
-<script>
-alert('invalid user name and password');
-window.location='login.php';
-</script>
-<?php
-}
-
+echo "End of PHP code.<br>";
 ?>
